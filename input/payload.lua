fa44e85479d1ae74057bac0cfa481adf4d5a2882
mwmw1221 <mwzdalne@gmail.com>
2026-05-29 14:12:46 +0200

    Remove hardcoded UID/GID in example script

--- input/payload.lua
+++ input/payload.lua
@@ -1,7 +1,7 @@
 local os = os
 
 -- source dbus env and show popup
-os.execute("(eval 'cat /tmp/envars.sh' && dbus-send --type=method_call --dest='com.harman.service.HMIGateWay' /com/harman/service/HMIGateWay com.harman.ServiceIpc.Invoke string:'displayGlobalPopup' string:'{\"timeout\":\"15000\",\"showonDisplayoff\":\"true\",\"showRunningCategory\":\"true\",\"msg\":\"Thanks, Harman.\\nuid=0(root) gid=0(root)\",\"title\":\"Software Update\"}') > /dev/null 2>&1 &")
+os.execute("(eval 'cat /tmp/envars.sh' && dbus-send --type=method_call --dest='com.harman.service.HMIGateWay' /com/harman/service/HMIGateWay com.harman.ServiceIpc.Invoke string:'displayGlobalPopup' string:'{\"timeout\":\"15000\",\"showonDisplayoff\":\"true\",\"showRunningCategory\":\"true\",\"msg\":\"Thanks, Harman.\\",\"title\":\"Software Update\"}') > /dev/null 2>&1 &")
 
 os.execute("sleep 1")
 
